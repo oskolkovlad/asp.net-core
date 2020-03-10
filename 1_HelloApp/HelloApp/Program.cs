f822e2e2bd3859d3bf21098fea232201682d8a41
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-10 19:04:32 +0300

    Classes Program and Startup

--- 1_HelloApp/HelloApp/Program.cs
+++ 1_HelloApp/HelloApp/Program.cs
@@ -13,10 +13,19 @@ namespace HelloApp
     {
         public static void Main(string[] args)
         {
+            // В методе Main вызывается метод у созданного объекта IHostBuilder вызывается метод Build(), который собственно
+            // создает хост - объект IHost, в рамках которого развертывается веб-приложение. А затем для непосредственного
+            // запуска у IHost вызывается метод Run.
+
             CreateHostBuilder(args).Build().Run();
+
+            // После этого приложение запущено, и веб-сервер начинает прослушивать все входящие HTTP-запросы.
         }
 
+        // Чтобы запустить приложение ASP.NET Core, необходим объект IHost, в рамках которого развертывается веб-приложение.
+        // Для создания IHost применяется объект IHostBuilder.
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+            // Непосредственно создание IHostBuilder производится с помощью метода Host.CreateDefaultBuilder(args).
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
--- 1_HelloApp/HelloApp/Startup.cs
+++ 1_HelloApp/HelloApp/Startup.cs
@@ -10,29 +10,60 @@ using Microsoft.Extensions.Hosting;
 
 namespace HelloApp
 {
+    // Класс Startup является входной точкой в приложение ASP.NET Core. Этот класс производит конфигурацию приложения,
+    // настраивает сервисы, которые приложение будет использовать, устанавливает компоненты для обработки запроса или middleware.
+
+    // Класс Startup должен определять метод Configure(), и также опционально в Startup можно
+    // определить конструктор класса и метод ConfigureServices().
     public class Startup
     {
+        IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        // Необязательный метод ConfigureServices() регистрирует сервисы, которые используются приложением.
+        // В качестве параметра он принимает объект IServiceCollection, который и представляет коллекцию сервисов в приложении.
+        // С помощью методов расширений этого объекта производится конфигурация приложения для использования сервисов.
+        // Все методы имеют форму Add[название_сервиса].
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
+        // Метод Configure устанавливает, как приложение будет обрабатывать запрос. Этот метод является обязательным.
+        // Для установки компонентов, которые обрабатывают запрос, используются методы объекта IApplicationBuilder.
+        // Объект IApplicationBuilder является обязательным параметром для метода Configure.
+
+        // Кроме того, метод нередко принимает еще один необязательный параметр - объект IWebHostEnvironment, который
+        // позволяет получить информацию о среде, в которой запускается приложение, и взаимодействовать с ней.
+        public void Configure(IApplicationBuilder app/*, IWebHostEnvironment env*/)
         {
-            if (env.IsDevelopment())
+            // если приложение в процессе разработки
+            if (_env.IsDevelopment())
             {
+                // то выводим информацию об ошибке, при наличии ошибки
                 app.UseDeveloperExceptionPage();
             }
 
+            // добавляем возможности маршрутизации
             app.UseRouting();
 
+            // устанавливаем адреса, которые будут обрабатываться
             app.UseEndpoints(endpoints =>
             {
+                // обработка запроса - получаем констекст запроса в виде объекта context
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    // отправка ответа в виде строки "Hello World!"
+                    await context.Response.WriteAsync($"Application name: {_env.ApplicationName}");
                 });
             });
         }
