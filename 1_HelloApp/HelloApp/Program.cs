28b27f9d429981d2d105516edd296f0e81fd1e83
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-16 23:13:23 +0300

    Work with static files

--- 1_HelloApp/HelloApp/Program.cs
+++ 1_HelloApp/HelloApp/Program.cs
@@ -24,7 +24,7 @@ namespace HelloApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<StaticFiles>();
-                    webBuilder.UseWebRoot("static");
+                    //webBuilder.UseWebRoot("static");
                 });
     }
 }
--- 1_HelloApp/HelloApp/static/StaticFiles.cs
+++ 1_HelloApp/HelloApp/static/StaticFiles.cs
@@ -1,16 +1,65 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace HelloApp
 {
+    // Статические файлы и работа с ними
     public class StaticFiles
     {
         public void Configure(IApplicationBuilder app)
         {
+            // Метод UseDirectoryBrowser
+
+            //app.UseDirectoryBrowser();
+            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")),
+
+            //    RequestPath = new PathString("/pages")
+            //});
+
+
+
+            // Файлы по умолчанию
+            /*
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear(); // удаляем имена файлов по умолчанию
+            options.DefaultFileNames.Add("content.html"); // добавляем новое имя файла
+            app.UseDefaultFiles(options); // установка параметров
+
+            //app.UseDefaultFiles();
+            */
+
+
+
+            /*
             app.UseStaticFiles();
-            
+
+            // Сопоставление каталогов с путями
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")),
+                RequestPath = new PathString("/pages")
+            });
+            */
+
+
+
+            // Метод UseFileServer() объединяет функциональность сразу всех трех вышеописанных методов
+            // UseStaticFiles, UseDefaultFiles и UseDirectoryBrowser
+            app.UseFileServer(new FileServerOptions
+            {
+                EnableDirectoryBrowsing = true,
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")),
+                RequestPath = new PathString("/pages"),
+                EnableDefaultFiles = false
+            });
+
+
+
             app.Run(async context =>
             {
                 await context.Response.WriteAsync("Hello World");
