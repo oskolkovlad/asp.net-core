6703b525ef5ccb4dca348ef34ce9eec3fd49ed54
oskolkovlad <oskolkovlad@yandex.ru>
2020-03-17 00:08:01 +0300

    Errors and HTTPS, HSTSP

---
+++ 1_HelloApp/HelloApp/Errors.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloApp
+{
+    public class Errors
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            env.EnvironmentName = "Production";
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+            // обработка ошибок HTTP
+            app.UseStatusCodePages();
+            app.UseStatusCodePagesWithRedirects("/error?code={0}");
+            app.UseStatusCodePagesWithReExecute("/error", "?code={0}");
+
+            app.Map("/error", ap => ap.Run(async context =>
+            {
+                await context.Response.WriteAsync($"Err: {context.Request.Query["code"]}");
+            }));
+
+            /*app.Map("/error", ap => ap.Run(async context =>
+            {
+                await context.Response.WriteAsync("DivideByZeroException occured!");
+            }));*/
+
+            /*app.Run(async context =>
+            {
+                var x = 0;
+                var y = 1 / x;
+                await context.Response.WriteAsync(y.ToString());
+            });*/
+        }
+    }
+}
---
+++ 1_HelloApp/HelloApp/Https.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace HelloApp
+{
+    public class Https
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 44344;
+            });
+
+            services.AddHsts(options =>
+            {
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.IncludeSubDomains = true;
+                options.Preload = true;
+                options.ExcludedHosts.Add("us.example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
